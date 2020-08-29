1592e4cb6e1dd31fef939079310b8955e724f5dd
Jos28c <jcjoseth09@gmail.com>
2020-08-29 16:31:16 -0600

    test 2

--- test/pageobjects/Result.page.js
+++ test/pageobjects/Result.page.js
@@ -2,15 +2,18 @@
 class Result{
 
     
-    get flightCheckbox(){return $('[data-Test=TransportOptionCheckbox-aircraft')}
-    get busCheckbox(){return $('[data-Test=TransportOptionCheckbox-bus')}
-    get trainCheckbox(){return $('[data-Test=TransportOptionCheckbox-train')}
+    get flightCheckbox(){return $('[data-Test=TransportOptionCheckbox-aircraft]')}
+    get busCheckbox(){return $('[data-Test=TransportOptionCheckbox-bus]')}
+    get trainCheckbox(){return $('[data-Test=TransportOptionCheckbox-train]')}
+    get belgicaCheck(){return $('//input[@type="checkbox"][@value="BE"]')}
+  
     get noResults(){return $('//div[@class="Heading__StyledHeading-sc-1b8cso5-0 fGFWje"]')}
 
     getFlightcheck(){return this.flightCheckbox;}
     getbuscheck(){return this.busCheckbox;}
     gettraincheck(){return this.trainCheckbox;}
     getnoresults(){return this.noResults;}
+    getbelgicacheck(){return this.belgicaCheck;}
 
     clicOnflightCheck()
     {
@@ -22,12 +25,16 @@ class Result{
     }
     clicOntrainCheck()
     {
-        this.flightCheckbox.click(); 
+        this.trainCheckbox.click(); 
     }
 
     getNoresultslabel()
     {
         return this.noResults;
+    }
+    clicOnBelgicaCheck()
+    {
+        this.belgicaCheck.click();
     }
 
 }   
